ae2a57a2a3011ad488874f18bed1a17a62ef9d1e
Bryan Reed <brian.reed.software@outlook.com>
2021-08-10 10:33:23 -0700

    updated styling

--- index.js
+++ index.js
@@ -4,26 +4,26 @@ function shake(){
   
 
   if (input.value == ''){
-  document.getElementById("console").style.fontSize = '20';
+  document.getElementById("console").style.fontSize = '5vw';
   document.getElementById("console").innerText = 'Please Enter Number';    
   }else if (input.value < 0){
     
-   document.getElementById("console").style.fontSize = '20px';
+   document.getElementById("console").style.fontSize = '5vw';
    document.getElementById("console").innerText = 'Positive Values Only';
    }else if (input.value % 15 === 0){ 
-    document.getElementById("console").style.fontSize = '36px';
+    document.getElementById("console").style.fontSize = '10vw';
     document.getElementById("console").innerText = "FIZZ BUZZ";
     document.getElementById('wrapper').className = 'shake';
   }else if (input.value % 2 === 0){
-  document.getElementById("console").style.fontSize = '36px';
+  document.getElementById("console").style.fontSize = '10vw';
   document.getElementById("console").innerText = 'FIZZ';
   document.getElementById('wrapper').className = 'shake';
   }else if(input.value % 3 === 0){
-  document.getElementById("console").style.fontSize = '36px';
+  document.getElementById("console").style.fontSize = '10vw';
   document.getElementById("console").innerText = 'BUZZ';
   document.getElementById('wrapper').className = 'shake';
   }else{
-    document.getElementById("console").style.fontSize = '20px';
+    document.getElementById("console").style.fontSize = '5vw';
     document.getElementById("console").innerText = 'No Fizz, No Buzz';
   }
     
