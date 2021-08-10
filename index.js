e3ccb5e9f44c51ae65540af6c846ae02ac96af45
Bryan Reed <brian.reed.software@outlook.com>
2021-08-10 10:09:01 -0700

    updated styling

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function shake(){
     
    if (input.value === undefined){
    document.getElementById("console").innerText = '?';
-   }else if (input.value % 15 === 0){ document.getElementById("console").innerText = alert(input);
+   }else if (input.value % 15 === 0){ document.getElementById("console").innerText = "FIZZ BUZZ";
   }else if (input.value % 2 === 0){
   docuwment.getElementById("console").innerText = 'FIZZ';
   }else if(input.value % 3 === 0){
