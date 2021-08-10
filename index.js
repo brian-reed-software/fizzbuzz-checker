a532c744d8008d34d4ea9deb3fc40fe065a3e2b7
Bryan Reed <brian.reed.software@outlook.com>
2021-08-10 10:36:58 -0700

    updated styling

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function shake(){
   document.getElementById("console").innerText = 'BUZZ';
   document.getElementById('wrapper').className = 'shake';
   }else{
-    document.getElementById("console").style.fontSize = '5vw';
+    document.getElementById("console").style.fontSize = '10vw';
     document.getElementById("console").innerText = 'No Fizz'
     setTimeout(function(){ 
     document.getElementById("console").innerText = 'No Buzz'
