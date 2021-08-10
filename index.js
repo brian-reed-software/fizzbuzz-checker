4c0575ec8e033708bb0ee44a04eddf452610f7f8
Bryan Reed <brian.reed.software@outlook.com>
2021-08-10 10:45:53 -0700

    updated styling

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ function shake(){
    document.getElementById("console").style.fontSize = '5vw';
    document.getElementById("console").innerText = 'Positive Values Only';
    }else if (input.value % 15 === 0){ 
-    document.getElementById("console").style.fontSize = '10vw';
+    document.getElementById("console").style.fontSize = '15vw';
     document.getElementById("console").innerText = "FIZZ BUZZ";
     document.getElementById('wrapper').className = 'shake';
   }else if (input.value % 2 === 0){
-  document.getElementById("console").style.fontSize = '10vw';
+  document.getElementById("console").style.fontSize = '15vw';
   document.getElementById("console").innerText = 'FIZZ';
   document.getElementById('wrapper').className = 'shake';
   }else if(input.value % 3 === 0){
-  document.getElementById("console").style.fontSize = '10vw';
+  document.getElementById("console").style.fontSize = '15vw';
   document.getElementById("console").innerText = 'BUZZ';
   document.getElementById('wrapper').className = 'shake';
   }else{
