c6b9840864ec03d2a5c7bb51b90cfb9c4ce9dc41
Bryan Reed <brian.reed.software@outlook.com>
2021-08-10 10:00:47 -0700

    updated styling

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 function shake(){
   
-    let input = document.querySelector("input")
+  let input = document.querySelector("input")
+  
+  document.getElementById('wrapper').className = 'shake';
+  
     
-    document.getElementById('wrapper').className = 'shake';
-      
-     if (input.value === '0' || null){
-     document.getElementById("console").innerText = input.value;
-     }else if (input.value % 15 === 0){ document.getElementById("console").innerText = alert(input);
-    }else if (input.value % 2 === 0){
-    docuwment.getElementById("console").innerText = 'FIZZ';
-    }else if(input.value % 3 === 0){
-    document.getElementById("console").innerText = 'BUZZ';
-    }else{
-      document.getElementById("console").innerText = ':(';
-    }
-      
-      setTimeout(function(){ document.getElementById('wrapper').className = 'noshake';
-    document.getElementById("console").innerText = '';
-    },2000)
+   if (input.value === '0' || null){
+   document.getElementById("console").innerText = input.value;
+   }else if (input.value % 15 === 0){ document.getElementById("console").innerText = alert(input);
+  }else if (input.value % 2 === 0){
+  docuwment.getElementById("console").innerText = 'FIZZ';
+  }else if(input.value % 3 === 0){
+  document.getElementById("console").innerText = 'BUZZ';
+  }else{
+    document.getElementById("console").innerText = ':(';
+  }
     
-    }
-    
+    setTimeout(function(){ document.getElementById('wrapper').className = 'noshake';
+  document.getElementById("console").innerText = '';
+  },2000)
+  
+  }
+  
