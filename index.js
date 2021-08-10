40edd25b7da8ea87772e147715053c27b31035d4
Bryan Reed <brian.reed.software@outlook.com>
2021-08-10 10:12:40 -0700

    updated styling

--- index.js
+++ index.js
@@ -2,16 +2,20 @@ function shake(){
   
   let input = document.querySelector("input")
   
-  document.getElementById('wrapper').className = 'shake';
+
   
     
    if (input.value === '' || 0){
    document.getElementById("console").innerText = '?';
-   }else if (input.value % 15 === 0){ document.getElementById("console").innerText = "FIZZ BUZZ";
+   }else if (input.value % 15 === 0){ 
+    document.getElementById("console").innerText = "FIZZ BUZZ";
+    document.getElementById('wrapper').className = 'shake';
   }else if (input.value % 2 === 0){
   docuwment.getElementById("console").innerText = 'FIZZ';
+  document.getElementById('wrapper').className = 'shake';
   }else if(input.value % 3 === 0){
   document.getElementById("console").innerText = 'BUZZ';
+  document.getElementById('wrapper').className = 'shake';
   }else{
     document.getElementById("console").innerText = ':(';
   }
