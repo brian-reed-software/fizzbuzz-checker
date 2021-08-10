6ae2974859149dde847c0bb87a29cf0665248b51
Bryan Reed <brian.reed.software@outlook.com>
2021-08-10 10:15:56 -0700

    updated styling

--- index.js
+++ index.js
@@ -3,15 +3,15 @@ function shake(){
   let input = document.querySelector("input")
   
 
-  
-    
-   if (input.value === '' || 0){
-   document.getElementById("console").innerText = '?';
+  if (input.value < 1){
+  document.getElementById("console").innerText = 'Please Enter Positive Number';    
+   else if (input.value === ''){
+   document.getElementById("console").innerText = 'No Value Entered';
    }else if (input.value % 15 === 0){ 
     document.getElementById("console").innerText = "FIZZ BUZZ";
     document.getElementById('wrapper').className = 'shake';
   }else if (input.value % 2 === 0){
-  docuwment.getElementById("console").innerText = 'FIZZ';
+  document.getElementById("console").innerText = 'FIZZ';
   document.getElementById('wrapper').className = 'shake';
   }else if(input.value % 3 === 0){
   document.getElementById("console").innerText = 'BUZZ';
